798c01af07fe3649e462e1c077e60df509c9eb08
GrantMLambert <glambert4110@gmail.com>
2022-04-11 15:23:31 -0500

    Updated database classes

--- Mobile App V2/Mobile App V2/Models/User.cs
+++ Mobile App V2/Mobile App V2/Models/User.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
 namespace Mobile_App_V2.Models
 {
     public class User
     {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
         public string UserName { get; set; }
+
+
 
     }
 }
---
+++ Mobile App V2/Mobile App V2/Services/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Essentials;
+using SQLite;
+using Mobile_App_V2.Models;
+
+namespace Mobile_App_V2.Services
+{
+    public static class User
+    {
+        static SQLiteAsyncConnection db;
+
+        static async Task Init()
+        {
+            if (db != null)
+                return;
+            
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "database.db");
+
+            db = new SQLiteAsyncConnection(databasePath);
+
+            await db.CreateTableAsync<Models.User>();
+
+
+        }
+
+        public static async Task AddUser(string name)
+        {
+            await Init();
+
+            var user = new Models.User
+            {
+                UserName = name
+            };
+
+            var id = await db.InsertAsync(user);
+        }
+
+        public static async Task RemoveUser(int id)
+        {
+            await Init();
+
+            await db.DeleteAsync<Models.User>(id);
+        }
+
+        public static async Task<IEnumerable<Models.User>> GetUser()
+        {
+            await Init();
+
+            var user = await db.Table<Models.User>().ToListAsync();
+
+            return user;
+        }
+
+        public static async Task UpdateUser(int id, string name)
+        {
+            await Init();
+
+            await db.UpdateAsync(
+                   new Models.User
+                   {
+                       id = id,
+                       UserName = name
+
+                   }
+               );
+        }
+    }
+
+}
